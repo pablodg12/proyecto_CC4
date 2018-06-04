719b20e855052a04cd90e172a64287bd8a65004e
Diego Pérez <diego.perez.11@alumnos.usm.cl>
2018-06-04 03:41:42 -0400

    dead state added to v2

--- agent_model_2.py
+++ agent_model_2.py
@@ -9,50 +9,69 @@ from copy import deepcopy
 #	- Resting: cell does not divides itself and does not consume resources (phase G0).
 class TumorCell():
     radius = 2.0
-    colors = ["red", "black", "green"]
+    colors = ["red", "orange", "green", "gray"]
   
-    def __init__(self, pos, consumption=4, interphase_length=10):
+    def __init__(self, pos, consumption=2, mitosis_threshold=10, apoptosis_threshold=10):
+    	# Parameters
+        self.consumption = consumption # resources consumed each timestep
+        self.mitosis_threshold = mitosis_threshold # resources needed to enter mitosis
+        self.apoptosis_threshold = apoptosis_threshold
+        # Variables
         self.pos = pos # initial position
         self.state = 0 # inital state
-        # Parameters
-        self.consumption = consumption # resources consumed each timestep
-        self.interphase_length = interphase_length # resources needed to enter mitosis
-        self.size = np.random.randint(0, self.interphase_length//2) # initial size (consumed resources)
+        self.reserve = apoptosis_threshold
+        self.size = np.random.randint(0, self.mitosis_threshold//2) # initial size (consumed resources)
 
     def cycle(self, tumor, resources, nbohrs):
-    	if self.state == 0:
+    	if self.state != 3:
+    		# Base consumption
     		if resources[self.pos] >= self.consumption:
     			resources[self.pos] -= self.consumption
-    			self.size += self.consumption
-    			if self.size >= self.interphase_length:
-    				self.state = 2 # Go to Mitosis phase
-    		else:
-    			self.size += resources[self.pos]
+    		elif self.reserve + resources[self.pos] >= self.consumption:
+    			self.reserve -= (self.consumption - resources[self.pos])
     			resources[self.pos] = 0
-    			self.state = 1 # Go to Resting phase
-
-    	elif self.state == 1:
-    		if self.size < self.interphase_length:
-    			if resources[self.pos] >= self.consumption:
-    				self.state = 0 # Go to interphase 
     		else:
+    			self.reserve = 0
+    			self.state = 3
+    			resources[self.pos] = 0
+    			return
+    		# State logic		
+    		if self.state == 0:
+    			if resources[self.pos] >= self.consumption:
+    				resources[self.pos] -= self.consumption
+    				self.size += self.consumption
+    			elif self.reserve + resources[self.pos] >= self.consumption:
+    				self.size += self.consumption
+    				self.reserve -= (self.consumption - resources[self.pos])
+    				resources[self.pos] = 0
+    			else:
+    				self.reserve = 0
+    				self.state = 3
+    				resources[self.pos] = 0
+    				return
+    			if self.size >= self.mitosis_threshold:
+    				self.state = 2 # Go to Mitosis phase
+    		elif self.state == 1:
     			nbohrs += self.pos
     			nbohrs[nbohrs < 0] = self.pos
     			nbohrs[nbohrs >= tumor.size] = self.pos
     			if np.any(tumor[nbohrs] == None):
     				self.state = 2
-    	else:
-    		nbohrs += self.pos
-    		nbohrs[nbohrs < 0] = self.pos
-    		nbohrs[nbohrs >= tumor.size] = self.pos
-    		mask = (tumor[nbohrs] == None)*resources[nbohrs]
-    		if not np.any(mask):
-    			self.state = 1
-    		else:
-    			new_pos = nbohrs[np.argmax(mask)]
-    			tumor[new_pos] = TumorCell(new_pos, consumption=self.consumption, interphase_length=self.interphase_length)
-    			self.size = 0
-    			self.state = 0
+    		elif self.state == 2:
+    			nbohrs += self.pos
+    			nbohrs[nbohrs < 0] = self.pos
+    			nbohrs[nbohrs >= tumor.size] = self.pos
+    			mask = (tumor[nbohrs] == None)*resources[nbohrs]
+    			if not np.any(mask):
+    				self.state = 1
+    			else:
+    				new_pos = nbohrs[np.argmax(mask)]
+    				tumor[new_pos] = TumorCell(new_pos, 
+    					consumption=self.consumption, 
+    					mitosis_threshold=self.mitosis_threshold, 
+    					apoptosis_threshold=self.apoptosis_threshold)
+    				self.size = np.random.randint(0, self.mitosis_threshold//2)
+    				self.state = 0
 
     def plot(self, N):
         x = 2*self.radius*(self.pos%N + ((self.pos//N)%2)/2)
@@ -70,8 +89,9 @@ class Tissue():
         # Model structures
         self.tumor = [np.empty(self.N**2, dtype=object)]
         self.tumor[-1][params["tumor_init_pos"]] = TumorCell(params["tumor_init_pos"], 
-        	consumption=params["tumor_cell_consumption"],
-        	interphase_length=params["tumor_cell_interphase_length"])
+        	consumption=params["tumorcell_consumption"],
+        	mitosis_threshold=params["tumorcell_mitosis_threshold"],
+        	apoptosis_threshold=params["tumorcell_apoptosis_threshold"])
         self.resources = [np.random.randint(params["resources_init_min"], params["resources_init_max"], self.N**2)]
 
     # Executes one timestep
@@ -79,8 +99,8 @@ class Tissue():
         self.resources.append(deepcopy(self.resources[-1]))
         self.tumor.append(deepcopy(self.tumor[-1]))
 
-        self.resources_flux()
         self.tumor_timestep()
+        self.resources_flux()
 
     # Returns the neighbors mask for a given cell with rigid boundary conditions.
     def get_tumor_nm(self, i):
@@ -102,11 +122,13 @@ class Tissue():
         return np.array(nm)
 
     def resources_flux(self):
+    	aux = deepcopy(self.resources[-1])
     	for i in range(0, self.N**2):
             if i < self.N or i > self.N**2 - self.N or i%self.N == 0 or i%self.N == self.N-1:
+                self.resources[-1][i] = self.resources[-2][i]
                 continue
             nbohrs = self.get_tumor_nm(i) + i
-            self.resources[-1][i] = self.resources[-2][i] + self.alpha/6*((self.resources[-2][nbohrs]).sum() - 6*self.resources[-2][i])
+            self.resources[-1][i] = aux[i] + self.alpha/6*((aux[nbohrs]).sum() - 6*aux[i])
 
     def tumor_timestep(self):
     	for cell in self.tumor[-1][self.tumor[-1] != None]:
